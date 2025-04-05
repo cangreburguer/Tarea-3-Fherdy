c3e1efccb27e6d447b731b33559d38a6c53f19b9
cangreburguer <fherdy24@gmail.com>
2025-04-04 21:16:28 -0400

    Modelo de producto

---
+++ CrudT3/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿namespace T3_Crud.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Precio:C}";
+        }
+    }
+}
