7d6959386589af26f2d935f21302b62aaeba017f
cangreburguer <fherdy24@gmail.com>
2025-04-04 21:47:38 -0400

    Función de cerrar form

---
+++ CrudT3/BotonCerrar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class BotonCerrar
+{
+    private Form form;
+
+    // Constructor que recibe el formulario
+    public BotonCerrar(Form form)
+    {
+        this.form = form;
+    }
+
+    // Método para cerrar el formulario
+    public void CerrarFormulario(object sender, EventArgs e)
+    {
+        form.Close();
+    }
+}
